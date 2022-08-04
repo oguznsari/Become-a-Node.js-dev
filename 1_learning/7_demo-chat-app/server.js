67c87d3d2a87e3d2fcb73bfc8f0a1497b8764399
oguzn.sari <oguzn.sari@gmail.com>
2022-08-04 23:47:49 +0300

    Async/await

--- 1_learning/7_demo-chat-app/server.js
+++ 1_learning/7_demo-chat-app/server.js
@@ -22,25 +22,21 @@ app.get('/messages', (req, res) => {
     })
 });
 
-app.post('/messages', (req, res) => {
+app.post('/messages', async (req, res) => {
     var message = new Message(req.body);
-
-    message.save()
-        .then(() => {
-            console.log('saved');
-            return Message.findOne({ message: 'badword' });
-        })
-        .then(censored => {
-            if (censored) {
-                console.log('censored words found: ', censored);
-                return Message.deleteOne({ _id: censored.id });
-            }
-            io.emit('message', req.body);
-            res.sendStatus(200);
-        })
-        .catch((err) => {
+    var savedMessage = await message.save();
+    console.log('saved');
+    
+    var censored = await Message.findOne({ message: 'badword' });
+    if(censored)
+        await Message.deleteOne({_id: censored.id});
+    else
+        io.emit('message', req.body);
+    
+    res.sendStatus(200);
+/*         .catch((err) => {
             res.sendStatus(500);
-        })
+        }) */
 });
 
 io.on('connection', (socket) => {
