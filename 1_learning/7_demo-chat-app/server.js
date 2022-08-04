58130f1a6c0bf736f62de09d693eeeaa6d1a3da0
oguzn.sari <oguzn.sari@gmail.com>
2022-08-04 21:35:34 +0300

    mongoose connection mLab

--- 1_learning/7_demo-chat-app/server.js
+++ 1_learning/7_demo-chat-app/server.js
@@ -3,10 +3,13 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+var dbUrl = 'mongodb+srv://osari:Kandy-1234@learning-node.lvucr7r.mongodb.net/?retryWrites=true&w=majority';
 
 var messages = [
     {name: 'Tim', message: 'Hi'},
@@ -25,6 +28,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+});
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('mongo db connection: ', err);
 });
 
 var server = http.listen(3000, () => {
