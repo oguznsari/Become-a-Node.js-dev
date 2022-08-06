d00351535c0f5848803529f558d5636d808aa393
oguzn.sari <oguzn.sari@gmail.com>
2022-08-06 11:39:11 +0300

    incorporate with setInterval

--- 2_essentials/5-timers.js
+++ 2_essentials/5-timers.js
@@ -1,6 +1,18 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+let incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} seconds`);
+}
+
 console.log(`setting a ${waitTime / 1000} seconds delay.`);
 
-const timerFinished = () => console.log('done');
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log('done');
+} 
 
 setTimeout(timerFinished, waitTime);
+const interval = setInterval(incTime, waitInterval);
