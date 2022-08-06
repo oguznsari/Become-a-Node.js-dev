e2c720f08c00f5f7f91a6cbab8293c2f4928c1a0
oguzn.sari <oguzn.sari@gmail.com>
2022-08-06 23:08:36 +0300

    readible file streams

---
+++ 2_essentials/8_files-and-streams/readStreams.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./assets/lorum-ipsum.md', 'utf-8');
+
+let fileTxt = '';
+
+console.log('type something...');
+readStream.on('data', data => {
+    console.log(`I read ${data.length - 1} characters of text.`);
+    fileTxt += data;
+});
+
+readStream.once('data', data => {
+    console.log('once data: ', data);
+});
+
+readStream.on('end', () => {
+    console.log('finished reading file.');
+    console.log(`In total I read ${fileTxt.length - 1} characters of text.`);
+})
