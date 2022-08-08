6f2ab1fb0feeefd093bf0e7d9d3a9f02a743cd4b
oguzn.sari <oguzn.sari@gmail.com>
2022-08-08 20:46:47 +0300

    eslint inheriting

---
+++ 4_testing-code-quality/nadia/public/.eslintrc.js
@@ -0,0 +1,14 @@
+({
+  "parserOptions": {
+    "sourceType": "script"
+  },
+  "env": {
+    "browser": true,
+    "jQuery": true,
+    "node": false
+  },
+  "rules": {
+    "no-var": "off",
+    "prefer-const": "off"
+  }
+})
