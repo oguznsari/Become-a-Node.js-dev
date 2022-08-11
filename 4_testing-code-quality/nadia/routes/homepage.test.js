909e8c287f76f82e28d4339ad0cabb4420170580
oguzn.sari <oguzn.sari@gmail.com>
2022-08-11 19:33:24 +0300

    isolate routes for faster tests

--- 4_testing-code-quality/nadia/routes/homepage.test.js
+++ 4_testing-code-quality/nadia/routes/homepage.test.js
@@ -1,61 +1,62 @@
 const request = require('supertest');
 
 let app;
+
 const mockMorgan = jest.fn((req, res, next) => next());
 const mockInsert = jest.fn().mockResolvedValue([1349]);
 
 beforeAll(() => {
+  jest.mock('./', () => require('./homepage'));
   jest.mock('morgan', () => () => mockMorgan);
   jest.mock('../lib/knex', () => () => ({
-    insert: mockInsert,
-  }));
-  jest.mock('./', () => require('./reservations'));
+    insert: mockInsert
+  }))
   app = request(require('../app'));
 });
 
 afterAll(() => {
-  jest.unmock('morgan');
   jest.unmock('./');
+  jest.unmock('morgan');
 });
 
-describe('GET', () => {
-  it('should return the reservations form', async () => {
+describe(`GET`, () => {
+  it(`should return the reservation form`, async () => {
     const response = await app.get('/reservations')
-      .expect('Content-Type', /html/)
+      .expect('Content-type', /html/)
       .expect(200);
 
-    expect(response.text).toContain('To make reservations please fill out the following form');
+    expect(response.text).toContain(`To make reservations please fill out the following form`);
   });
 });
 
-describe('POST', () => {
-  it('should reject an invalid reservation request', async () => {
+describe(`POST`, () => {
+  it(`should reject an invalid reservation request`, async () => {
     const response = await app.post('/reservations')
       .type('form')
       .send({
         date: '2017/06/10',
         time: '06:02 AM',
-        party: 'bananas',
-        name: 'Family',
+        party: 'bananas',      // invalid
+        name: 'family',
         email: 'username@example.com'
       });
 
-    expect(response.text).toContain('Sorry, there was a problem with your booking request.');
-    expect(response.status).toBe(400);
+      expect(response.text).toContain(`Sorry, there was a problem with your booking request.`);
+      expect(response.status).toBe(400);
   });
 
-  it('should accept a valid reservation request', async () => {
+  it(`should accept a valid reservation request`, async () => {
     const response = await app.post('/reservations')
       .type('form')
       .send({
         date: '2017/06/10',
         time: '06:02 AM',
-        party: 8,
-        name: 'Family',
+        party: '4',
+        name: 'family',
         email: 'username@example.com'
       })
       .expect(200);
 
-    expect(response.text).toContain('Thanks, your booking request #1349');
-  });
+    expect(response.text).toContain(`Thanks, your booking request #1349`);
+  })
 });
