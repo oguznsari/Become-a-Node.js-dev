c53a01848e7900348fa3a80904c32c8ea8fe3218
oguzn.sari <oguzn.sari@gmail.com>
2022-08-13 17:23:54 +0300

    HTTP messages, methods and routes

--- 6_express-essential-t/express-essentials/index.js
+++ 6_express-essential-t/express-essentials/index.js
@@ -5,6 +5,27 @@ const app = express();
 
 const PORT = 3000;
 
+
+// GET
+app.get('/', (req, res) => {
+  res.send('This is a GET request at /');
+});
+
+// POST
+app.post('/create', (req, res) => {
+  res.send('This is a POST request at /create');
+});
+
+// PUT
+app.put('/edit', (req, res) => {
+  res.send('This is a PUT request at /edit');
+});
+
+// DELETE
+app.delete('/delete', (req, res) => {
+  res.send('This is a DELETE request at /delete');
+});
+
 app.listen(PORT, () => {
   console.log(`The server is running on port: ${PORT}!`);
   console.log(data);
