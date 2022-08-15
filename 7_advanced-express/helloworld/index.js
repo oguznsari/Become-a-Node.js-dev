759de65052d5d4b05f738a2613470cf5b6f5969f
oguzn.sari <oguzn.sari@gmail.com>
2022-08-15 09:08:43 +0300

    create template engine for express

--- 7_advanced-express/helloworld/index.js
+++ 7_advanced-express/helloworld/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const fs = require('fs');
+const util = require('util');
+const marked = require('marked');
 const app = express();
 const PORT = 3000;
 
+const fsreadFile = util.promisify(fs.readFile);
+
+app.engine('md', async (filePath, options, callback) => {
+  try {
+    const content = await fsreadFile(filePath);
+    const rendered = content.toString().replace('{headline}', options.headline);
+    return callback(null, marked.parse(rendered));
+  } catch (err) {
+    callback(err);
+  }
+});
+
+app.set('views', 'views');
+app.set('view engine', 'md');
+
 function handler (req, res) {
-  return res.send('Hello World!');
+  return res.render('index', { headline: 'Hello World!' });
 }
 
 app.get('/', handler);
---
+++ 7_advanced-express/helloworld/views/index.md
@@ -0,0 +1,4 @@
+# {headline}
+* List Item 1
+* **List Item 2**
+* __List Item 3__
