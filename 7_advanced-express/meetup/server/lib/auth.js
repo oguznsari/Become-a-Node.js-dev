975b9caa55798e4a55234e7a8bc979d230d372ac
oguzn.sari <oguzn.sari@gmail.com>
2022-08-23 22:09:19 +0300

    set up Auth strategy: Local

--- 7_advanced-express/meetup/server/lib/auth.js
+++ 7_advanced-express/meetup/server/lib/auth.js
@@ -1,4 +1,22 @@
 const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const UserModel = require('../models/UserModel');
+
+passport.use(new localStrategy({ usernameField: 'email' }, async (username, password, done) => {
+  try {
+    const user = await UserModel.findOne({email: username}).exec();
+    if(!user) {
+      return done(null, false, { message: 'Invalid username or password.' });
+    }
+    const passwordOK = await user.comparePassword(password);
+    if(!passwordOK) {
+      return done(null, false, { message: 'Invalid username or password.' });
+    }
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+}))
 
 module.exports = {
   initialize: passport.initialize(),
