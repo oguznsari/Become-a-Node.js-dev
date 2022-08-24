0481b905a2a2ef0e12a0b07ac1d156e4feb612f0
oguzn.sari <oguzn.sari@gmail.com>
2022-08-24 23:48:22 +0300

    bugfix for img

--- 7_advanced-express/meetup/server/routes/middlewares.js
+++ 7_advanced-express/meetup/server/routes/middlewares.js
@@ -13,6 +13,6 @@ module.exports.handleAvatar = avatars => async(req, res, next) => {
   if(req.file.mimetype !== 'image/png' && req.file.mimetype !== 'image/jpeg') {
     return next(new Error('File format is not supported.'));
   }
-  req.file.storedFileName = await avatars.store(req.file.buffer);
+  req.file.storedFilename = await avatars.store(req.file.buffer);
   return next();
 };
