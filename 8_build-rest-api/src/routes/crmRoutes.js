58c515ad50b83d8f5b235bbd7a5067181cdad8a8
oguzn.sari <oguzn.sari@gmail.com>
2022-09-06 21:51:32 +0300

    8 -> getContactById

--- 8_build-rest-api/src/routes/crmRoutes.js
+++ 8_build-rest-api/src/routes/crmRoutes.js
@@ -1,4 +1,4 @@
-import { addNewContact, getContacts } from '../controllers/crmController';
+import { addNewContact, getContacts, getContactWithId } from '../controllers/crmController';
 
 const routes = (app) => {
   app.route('/contact')
@@ -12,6 +12,8 @@ const routes = (app) => {
     .post(addNewContact);
 
   app.route('/contact/:contactID')
+    .get(getContactWithId)
+
     .put((req, res) =>
     res.send('PUT request successful!'))
 
